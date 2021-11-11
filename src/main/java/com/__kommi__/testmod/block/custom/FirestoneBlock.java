aa8817825ba54424cc0e9f6c50469cb0a1b0d002
shinyangdevelop <shinyangdevelop@gmail.com>
2021-11-11 23:18:14 +0900

    Add custom particles to FirestoneBlock

--- src/main/java/com/__kommi__/testmod/block/custom/FirestoneBlock.java
+++ src/main/java/com/__kommi__/testmod/block/custom/FirestoneBlock.java
@@ -4,11 +4,16 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particles.BlockParticleData;
+import net.minecraft.particles.ParticleType;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class FirestoneBlock extends Block {
     public FirestoneBlock(Properties properties) {
@@ -28,6 +33,24 @@ public class FirestoneBlock extends Block {
             }
         }
         return super.onBlockActivated(state, worldIn, pos, player, handIn, hit);
+    }
+
+    @Override
+    public void animateTick(BlockState p_180655_1_, World p_180655_2_, BlockPos p_180655_3_, Random p_180655_4_) {
+        float chance = 0.25f;
+
+        if(chance < p_180655_4_.nextFloat()) {
+            p_180655_2_.addParticle(ParticleTypes.FLAME, p_180655_3_.getX() + p_180655_4_.nextDouble(),
+                    p_180655_3_.getY(), p_180655_3_.getZ()+p_180655_4_.nextDouble(),
+                    0d, 0.05d, 0d);
+
+            p_180655_2_.addParticle(new BlockParticleData(ParticleTypes.BLOCK, p_180655_1_), p_180655_3_.getX()+p_180655_4_.nextDouble(),
+                    p_180655_3_.getY(), p_180655_3_.getZ()+p_180655_4_.nextDouble(),
+                    0.0d, 0.05d, 0.0d);
+        }
+
+
+        super.animateTick(p_180655_1_, p_180655_2_, p_180655_3_, p_180655_4_);
     }
 
     @SuppressWarnings("deprecation")
